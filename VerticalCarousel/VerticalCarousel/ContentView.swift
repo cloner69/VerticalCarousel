06681804be4006fe2d17659bc89b0fe7214aedaf
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 02:44:50 +0700

    vertical carousel

---
+++ VerticalCarousel/VerticalCarousel/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  VerticalCarousel
+//
+//  Created by Adrian Suryo Abiyoga on 10/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack{
+            Home()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VerticalCarousel/VerticalCarousel/VerticalCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalCarouselApp.swift
+//  VerticalCarousel
+//
+//  Created by Adrian Suryo Abiyoga on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct VerticalCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
